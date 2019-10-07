108f8c8869174d51442b7e3d0fd720a3d8760c81
Kirill Shcherbina <rabbichaim770@gmail.com>
2019-10-07 03:22:31 +0000

    costumercontroller fix

--- app/controllers/costumers_controller.rb
+++ app/controllers/costumers_controller.rb
@@ -2,32 +2,64 @@ class CostumersController < ApplicationController
 
 
   get "/costumers" do
-    erb :"/costumers/index.html"
+    if !logged_in?
+       redirect "/users/login"
+     else
+       @user = User.find(session[:user_id])
+       erb :"/costumers/index.html"
+    end
   end
 
   
   get "/costumers/new" do
-    erb :"/costumers/new.html"
+     if !logged_in?
+       redirect to "/users/login"
+     else
+       erb :"/costumers/new.html"
+     end
   end
 
   
   post "/costumers" do
-    redirect "/costumers"
+    if params[:name] !=""
+      @user = User.find(session[:user_id])
+      @user.name << Costumer.create(name: params[:name])
+      redirect "/costumers"
+    else
+      redirect "/costumers/new"
+    end
   end
 
   # GET: /costume_rs/5
   get "/costumers/:id" do
-    erb :"/costumers/show.html"
+    if !logged_in?
+      redirect "/users/login"
+    else
+      @costumer = Costumer.find(params[:id])
+      erb :"/costumers/show.html"
+    end
   end
 
   # GET: /costume_rs/5/edit
   get "/costumers/:id/edit" do
-    erb :"/costumers/edit.html"
+    if !logged_in?
+      redirect "/users/login"
+    else
+      @costumer = Costumer.find(params[:id])
+      erb :"/costumers/edit.html"
+    end
   end
 
   # PATCH: /costume_rs/5
   patch "/costumers/:id" do
-    redirect "/costumers/:id"
+      costumer = Costumer.find(params[:id])
+         if params[:content] != ""
+           costumer.update(name: params[:name])
+           costumer.save
+           redirect "/costumers/#{costumer.id}"
+         else
+           redirect "/costumers/#{costumer.id}/edit"
+         end
   end
 
   # DELETE: /costume_rs/5/delete
